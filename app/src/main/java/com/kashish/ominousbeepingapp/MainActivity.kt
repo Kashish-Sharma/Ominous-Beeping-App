02fc2136e63ed5492366bc681ffc1753bc407d21
Kashish Sharma <kashish25798@gmail.com>
2019-07-21 02:26:20 +0530

    Add: Screen awake

--- app/src/main/java/com/kashish/ominousbeepingapp/MainActivity.kt
+++ app/src/main/java/com/kashish/ominousbeepingapp/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import com.kashish.ominousbeepingapp.databinding.ActivityMainBinding
@@ -40,9 +41,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
-
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         initSensorManager()
-
     }
 
     private fun initSensorManager() {
